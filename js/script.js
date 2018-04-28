a9105a295e2934908b50484fb74116e3d4c96c5c
gpmdev <info@gpmdev.it>
2018-04-28 21:22:28 +0200

    enh(): work complete

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 150)
+           scrollTop: (target.offset().top - 200)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -23,7 +23,7 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 57
+    offset: 200
   });
 
   // Collapse Navbar
@@ -49,22 +49,6 @@
   sr.reveal('.sr-button', {
     duration: 1000,
     delay: 200
-  });
-
-  // Magnific popup calls
-  $('.popup-gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
   });
 
 })(jQuery); // End of use strict
